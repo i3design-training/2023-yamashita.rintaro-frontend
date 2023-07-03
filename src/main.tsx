208a1f707dbba27fc61d117b97518f0c5e23452a
yamashita.rintaro <yamashita.rintaro@i3design.co.jp>
2023-07-03 16:29:22 +0900

    feat: TODOリストの最小限マークアップ完了

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,21 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Tasks from './pages/Task/Task';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
   },
+  {
+    path: '/tasks',
+    element: <Tasks />,
+  },
 ]);
 
 const container = document.getElementById('root');
+
 if (container) {
   ReactDOM.createRoot(container).render(
     <React.StrictMode>
