e433cd42489b28987de06ddb5091200fded44e66
yamashita.rintaro <yamashita.rintaro@i3design.co.jp>
2023-07-10 15:31:50 +0900

    feat: ログイン時にuser_idをローカルストレージに保存する

--- src/context/TokenContext.tsx
+++ src/context/TokenContext.tsx
@@ -10,10 +10,21 @@ import {
 } from 'react';
 
 // トークンとその更新関数を持つcontextを作成。デフォルト値は空のトークンと空関数
-const TokenContext = createContext<[string, Dispatch<SetStateAction<string>>]>([
+const TokenContext = createContext<
+  [
+    string,
+    Dispatch<SetStateAction<string>>,
+    string,
+    Dispatch<SetStateAction<string>>,
+  ]
+>([
   '',
   () => {
     throw new Error('setToken function must be overridden');
+  },
+  '',
+  () => {
+    throw new Error('setUserId function must be overridden');
   },
 ]);
 
@@ -23,15 +34,17 @@ type TokenProviderProps = {
 
 export const TokenProvider: FC<TokenProviderProps> = ({ children }) => {
   const [token, setToken] = useState(localStorage.getItem('token') ?? '');
+  const [userId, setUserId] = useState(localStorage.getItem('userId') ?? '');
 
   // 再ログイン時など、トークンが変更されたときにlocalStorageを更新
   useEffect(() => {
     localStorage.setItem('token', token);
-  }, [token]);
+    localStorage.setItem('userId', userId);
+  }, [token, userId]);
 
   return (
     // 子コンポーネントからトークンを取得・更新可能に
-    <TokenContext.Provider value={[token, setToken]}>
+    <TokenContext.Provider value={[token, setToken, userId, setUserId]}>
       {children}
     </TokenContext.Provider>
   );
